4644b55740d0232fa355be11069916e5da8f9156
Russo-ow <e10benga@gmail.com>
2021-01-09 17:40:46 -0600

    NPCs move in straight lines

--- Hubgrub Unity/Assets/Scripts/NpcController.cs
+++ Hubgrub Unity/Assets/Scripts/NpcController.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NpcController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class NpcController : MonoBehaviour {
+    public Direction direction = Direction.Up;
+
+    Rigidbody2D rb;
+    float speed;
+
+    public enum Direction {
+        Up,
+        Down,
+        Left,
+        Right
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        speed = PlayerController.instance.speed;
+    }
+
+    void Update() {
+        rb.velocity = Dir2Vector(direction);
+    }
+
+    Vector2 Dir2Vector(Direction d) {
+        switch(d) {
+            case Direction.Up:
+                return Vector2.up;
+            case Direction.Down:
+                return Vector2.down;
+            case Direction.Left:
+                return Vector2.left;
+            case Direction.Right:
+                return Vector2.right;
+            default:
+                return Vector2.zero;
+        }
     }
 }
