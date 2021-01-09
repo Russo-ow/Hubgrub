ad267f7891addd962d189250f2723ac267c2dfd6
MindyV21 <mmv0321@gmail.com>
2021-01-09 17:35:19 -0600

    player

---
+++ Hubgrub Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public static PlayerController instance;
+
+    Rigidbody2D rb;
+    public float speed;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Awake() {
+        instance = this;
+    }
+
+    void Update() {
+        rb.velocity = speed * new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+    }
+}
