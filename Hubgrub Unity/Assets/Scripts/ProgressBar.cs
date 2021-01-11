42051579e1c052c2e2d17f001d9b650543bd7100
MindyV21 <mmv0321@gmail.com>
2021-01-10 20:21:41 -0600

    final game maze tilemap done

---
+++ Hubgrub Unity/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ProgressBar : MonoBehaviour {
+    public static ProgressBar instance;
+
+    public Slider slider;
+
+    IEnumerator Start() {
+        slider.value = slider.maxValue;
+
+        while (slider.value > 0.0f) {
+            yield return new WaitForSeconds(1.0f);
+            slider.value -= 10.0f;
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Awake() {
+        instance = this;
+    }
+
+    // testing purposes
+    private void Update() {
+        if (Input.anyKeyDown) {
+            slider.value += 5.0f;
+        }    
+    }
+}
